a1ead2e85c13757b9683cde49f934868515f1d74
parksoyoung <opaldust19@gmail.com>
2024-03-20 14:19:30 +0900

    최소값

--- 0_세수중최소값.js
+++ 0_세수중최소값.js
@@ -1,9 +1,25 @@
 function solution(a, b, c){
   let answer;
-  if(a<b) answer=a;
+  function compareAB(m,n){
+    // console.log(m+'>'+n);
+    m>n ? answer=n:answer=m;
+  // console.log(answer+"::");
+ 
+  }
+  compareAB(a,b);
+  compareAB(a,c);
+  compareAB(b,c);
+
+  /*if(b>c){
+    answer= c
+  } else{answer=b} 
+  if(a<c){
+    answer= a
+  }else{answer=c}*/
+  /*if(a<b) answer=a;
   else answer=b;
-  if(c<answer) answer=c; 
+  if(c<answer) answer=c; */
   return answer;
 }
-
-console.log(solution(2, 5, 1));
+// console.log("start::");
+console.log(solution(3, 2, 4));
