6c36515f294ce505a2b4431e8046ddaec49bbefa
handcraft <opaldust19@gmail.com>
2024-03-29 23:06:22 +0900

    ㅇ

--- 일곱난장이.js
+++ 일곱난장이.js
@@ -1,7 +1,18 @@
 function solution(arr){
-    
+    let answer=arr;
+    for(let x of arr){
+        const xIndex=answer.indexOf("red");
+        for(let j of arr){
+            const jIndex=answer.indexOf("red");
+            if(x+j == 40 && j!=x){
+               
+                answer.splice(xIndex,1);
+                answer.splice(jIndex,1);
+            }
+        }
+    }
     return answer;
 }
 
-let arr=[22, 7, 21, 19, 10, 15, 25, 8, 13];
+let arr=[20, 7, 23, 19, 10, 15, 25, 8, 13];
 console.log(solution(arr));
