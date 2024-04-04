996a57c04101f12f28d4bf678bcaca339835e76a
handcraft <opaldust19@gmail.com>
2024-04-04 16:56:15 +0900

    subString

--- 0_가운데문자출력.js
+++ 0_가운데문자출력.js
@@ -1,8 +1,20 @@
 let solution =(s) =>{
-    let arr =s.toUpperCase();
-    return arr;
+    let answer = "";
+    let mid = Math.floor(s.length/2)//해설봄 여기
+
+    if(s.length%2 == 0){
+        //answer = s.substring(mid-1,mid+1);
+        answer = s.substr(mid-1,2);
+    }else{
+        //answer = s.substring(mid,mid+1);
+        answer = s.substr(mid ,1);
+    }
+    return answer;
 }
+//substr(인덱스,출력갯수)
+//substring (출력갯수,인덱스)
+//Math.floor(x);내림
+//Math.ceil(x);올림
+//Math.round(x)::숫자에서 가장 가까운 정수를 반환합니다.
 
-
-let str="ItisTimeToStudy";
-console.log(solution(str));
+console.log(solution("goodd"));
