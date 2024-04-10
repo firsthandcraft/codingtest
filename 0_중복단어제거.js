7440b9aa55413fc78537c82251e0a43b86adb9c2
handcraft <opaldust19@gmail.com>
2024-04-10 14:09:49 +0900

    f

--- 0_중복단어제거.js
+++ 0_중복단어제거.js
@@ -1,6 +1,5 @@
 function solution(s){  
     let answer;
-   
     answer=s.filter(function(v, i){
         //console.log(v,i);
         if(s.indexOf(s[i])=== i){
@@ -12,4 +11,10 @@ function solution(s){
     return answer;
 }
 let str=["good", "time", "good", "time", "student"];
-console.log(solution(str));
+//console.log(solution(str));
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //최댓값은 제외, 최솟값은 포함
+}
+console.log(getRandomInt(3,5) );
