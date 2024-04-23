b4c6c4c5dcedbfd65ce889042b45b4334fa02fb1
handcraft <opaldust19@gmail.com>
2024-04-23 15:38:16 +0900

    dd

--- LV_2_숫자만추출.js
+++ LV_2_숫자만추출.js
@@ -1,12 +1,20 @@
 
 function solution(s){
   let answer="";
-  s=s.toLowerCase().replace(/[^a-z]/g,'');
     for(let x of str){
       if(!isNaN(x)){answer+=x;}
     }
     
     return parseInt(answer);
 }
+
+function solution2(s){
+  let answer=0;//숫자일때
+    for(let x of str){
+      if(!isNaN(x)){answer = answer*10+Number(x)}
+    }
+    
+    return answer;
+}
 let str="g0en2T0s8eSoft";
-            console.log(solution(str));
+            console.log(solution2(str));
