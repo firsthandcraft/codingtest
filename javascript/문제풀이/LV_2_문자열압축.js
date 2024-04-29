accc4822b0c47dd7ab8b45bba2f87d8520e57866
handcraft <opaldust19@gmail.com>
2024-04-29 17:27:44 +0900

    29일

--- javascript/문제풀이/LV_2_문자열압축.js
+++ javascript/문제풀이/LV_2_문자열압축.js
@@ -1,26 +1,37 @@
 
-function solution(s,f){
+function solution1(s){
   let answer=[];
-  let p= 1000;
+  let p= 1;
+  let m= '';
     for(let x of s){
-      if(x == f){
-        p=0;
-        answer.push(p);
-      } else{
+      if(x==m){
         p++;
         answer.push(p);
+        
+      }else{
+        p=1;
+        m=x;
+        answer.push(x);
       }
     }
-    for(let i=s.length-1; i>=0;i--){
-      if(s[i] == f){
-        p=0;
-        // answer[i]=0;
-      } else{
-        p++;
-        answer[i] = Math.min(answer[i],p)
-      }
-    }
+    
     return answer;
 }
-let str="teachermode";
-console.log(solution(str, 'e'));
+
+function solution(s){
+  let answer="";
+  let cnt=1;
+  s=s+" ";
+  
+  for(let i=0; i<s.length-1; i++){
+      if(s[i]===s[i+1]) cnt++;
+      else{
+          answer+=s[i];
+          if(cnt>1) answer+=String(cnt);
+          cnt=1;
+      }
+  }
+  return answer;
+}
+let str="KKHSSSSSSSE";
+console.log(solution(str));
